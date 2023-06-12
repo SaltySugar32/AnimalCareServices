6cb8cfed355bccae3a5d954d573bc633ece9bcc1
Vitaly <dcd32032@gmail.com>
2023-06-12 10:51:23 +1000

    css and template update

--- CODE/controllers/about_service.py
+++ CODE/controllers/about_service.py
@@ -1,12 +1,12 @@
 from app import app
 from utils import get_db_connection
 from flask import render_template
-from models.master_service_model import get_master_service
+from models.about_service_model import get_master_service
 
 @app.route('/service/<id>')
 def master_service(id):
     con = get_db_connection()
     master_service = get_master_service(con, id)
-    html = render_template('master_service.html', title='Услуга', content = master_service.to_html(index=False))
+    html = render_template('about_service.html', title='Услуга', content = master_service.to_html(index=False))
     con.close()
     return html  
