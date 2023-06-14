e5f34ae36ab97c9010f657e901746161bde42ea8
Vitaly <dcd32032@gmail.com>
2023-06-14 14:17:25 +1000

    visual update

--- CODE/app.py
+++ CODE/app.py
@@ -1,7 +1,10 @@
-from flask import Flask
+from flask import Flask, session
 
 app = Flask(__name__)
 app.secret_key = b'supersecretkey'
+
+# без учеток пока
+session['client_id'] = 21
 
 import controllers.index
 import controllers.about_service
--- CODE/controllers/order_checkout.py
+++ CODE/controllers/order_checkout.py
@@ -1,16 +1,19 @@
 from app import app
-from flask import render_template, session
+from flask import render_template, session, request
 from models.order_checkout_model import get_order_list
 from utils import get_db_connection
 
 @app.route('/order')
 def order_checkout():
     con = get_db_connection()
-    table = get_order_list(con, session['m_service_id'])
+    m_service_id = session['m_service_id']
+    date = request.values.get('order_date')
+    table = get_order_list(con, m_service_id)
     html = render_template(
         'order_checkout.html', 
         title = 'Заказ',
-        content = table
+        content = table,
+        date = date
         )
     con.close()
     return html
